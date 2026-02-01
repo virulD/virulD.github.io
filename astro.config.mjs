036cd436d9fb81e9e4b575ba0a95fdf4c4a16640
Virul Akbo De Silva <waviruldesilva@gmail.com>
2026-02-01 23:56:43 +0530

    made some changes in project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import process from 'node:process';
+
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://virulD.github.io',
-    base: process.env.GITHUB_ACTIONS ? '/virulD.github.io/' : '/',
+    base: '/virulD.github.io/',
 });
