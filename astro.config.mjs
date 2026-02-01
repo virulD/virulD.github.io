45ea63378620f2b8be5357972346942411ee01c3
Virul Akbo De Silva <waviruldesilva@gmail.com>
2026-02-01 19:37:56 +0530

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://github.com/virulD.github.io',
-    base: '/astro-portfolio/',
+    base: '/virulD.github.io/',
 });
