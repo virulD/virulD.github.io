05d65b04c583e15129090a7c959205d09f0fcde0
Virul Akbo De Silva <waviruldesilva@gmail.com>
2026-09-08 10:30:25 +0530

    fixed the url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://virulD.github.io',
-    base: '/',
+  site: "https://viruldesilva.github.io",
+  base: "/virulD.github.io",
 });
